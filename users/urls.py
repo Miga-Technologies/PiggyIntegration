4aa671096dabace3f087de8458ac3bf043815086
Thiago Medeiros <thia.med@hotmail.com>
2025-06-29 17:26:23 -0300

    Falta finalizar testes

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import firebase_login,firebase_register
 
 urlpatterns = [
-    path('/auth/register/',firebase_register, name='firebase_register'),
-    path('/auth/login/',firebase_login,name='firebase_login'),
+    path('auth/register/',firebase_register, name='firebase_register'),
+    path('auth/login/',firebase_login,name='firebase_login'),
 ]
